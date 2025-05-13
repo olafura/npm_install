2808a0b4f9e9aeefe61ab3fb37609f6174ffd49d
Olafur Arason <olafura@olafura.com>
2025-05-13 16:21:00 +0200

    chore: update mix file

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule NpmInstall.Mixfile do
       app: :npm_install,
       version: "0.1.0",
       elixir: "~> 1.7",
+      description: "Run npm install",
+      package: package(),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -23,6 +25,15 @@ defmodule NpmInstall.Mixfile do
     [
       {:ex_doc, "~> 0.10", only: :dev},
       {:mock, "~> 0.3.0", only: :test}
+    ]
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*", "LICENSE*"],
+      maintainers: ["Olafur Arason"],
+      licenses: ["Apache-2.0"],
+      links: %{"GitHub" => "https://github.com/olafura/npm_install"}
     ]
   end
 end
